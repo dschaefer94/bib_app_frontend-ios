a457897308b57980bd8470c14ebdfe21cd891332
Daniel Schäfer / PBD2H24A <pbd2h24asc@M18.local>
2026-05-28 12:58:27 +0200

    Core-Data-Entwurf

---
+++ bib-app/bib-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bib-app
+//
+//  Created by Daniel Schäfer / PBD2H24A on 28.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bib-app/bib-app/bib_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bib_appApp.swift
+//  bib-app
+//
+//  Created by Daniel Schäfer / PBD2H24A on 28.05.26.
+//
+
+import SwiftUI
+
+@main
+struct bib_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
